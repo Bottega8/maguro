6ff0ecad62d0e062caa057cc3c090b197d3e7aa5
Haji Furukawa <haji@bottega8.com>
2015-01-08 17:41:49 -0800

    Update to latest ruby and rails versions

--- lib/maguro/version.rb
+++ lib/maguro/version.rb
@@ -2,6 +2,6 @@ module Maguro
   # Gem version
   VERSION = '0.0.1'
   # Version of requirements
-  RUBY_VERSION = '2.1.5'
-  RAILS_VERSION = '4.1.8'
+  RUBY_VERSION = '2.2.0'
+  RAILS_VERSION = '4.2.0'
 end
